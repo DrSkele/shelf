d18a24968a4756425699f93c4ed6f20ee5997e34
SeungSoo <skele1207@gmail.com>
2025-05-05 22:46:56 +0900

    fix(UseCase)

--- core/domain/src/main/java/com/skele/core/domain/TimerSettingsUseCase.kt
+++ core/domain/src/main/java/com/skele/core/domain/TimerSettingsUseCase.kt
@@ -2,19 +2,18 @@ package com.skele.core.domain
 
 import com.skele.core.data.repository.TimerSettingsRepository
 import com.skele.core.model.TimerSettings
-import com.skele.core.model.toEntity
 import javax.inject.Inject
 
 class TimerSettingsUseCase @Inject constructor(
     private val timerSettingsRepository: TimerSettingsRepository,
 ) {
-    suspend fun getSettingsList() = timerSettingsRepository.getSettingsList()
+    fun getSettingsList() = timerSettingsRepository.getSettingsList()
 
-    suspend fun getSettings(id: Long) = timerSettingsRepository.getSettings(id)
+    fun getSettings(id: Long) = timerSettingsRepository.getSettings(id)
 
-    suspend fun insertSettings(settings: TimerSettings) = timerSettingsRepository.insertSettings(settings.toEntity())
+    suspend fun insertSettings(settings: TimerSettings) = timerSettingsRepository.insertSettings(settings)
 
-    suspend fun updateSettings(settings: TimerSettings) = timerSettingsRepository.updateSettings(settings.toEntity())
+    suspend fun updateSettings(settings: TimerSettings) = timerSettingsRepository.updateSettings(settings)
 
-    suspend fun deleteSettings(settings: TimerSettings) = timerSettingsRepository.deleteSettings(settings.toEntity())
+    suspend fun deleteSettings(settings: TimerSettings) = timerSettingsRepository.deleteSettings(settings)
 }
