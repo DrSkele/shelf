cd85bbddc68b5c0d4d47e62510d266b0cafe4d50
SeungSoo <skele1207@gmail.com>
2025-08-03 15:52:48 +0900

    Remove existing modules to refactor project structure

--- feature/timer/src/main/java/com/skele/feature/timer/TimerScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.skele.feature.timer
-
-import androidx.compose.ui.Modifier
-
-fun TimerScreen(
-    modifier: Modifier = Modifier
-) {
-
-}
--- feature/todolist/src/main/java/com/skele/todolist/ToDoListViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.skele.todolist
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class ToDoListViewModel @Inject constructor() : ViewModel() {
-
-}
